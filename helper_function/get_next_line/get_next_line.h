b8f400d7c4571c05eacbe99971dbc97d9f28f3f1
Abderrahim Ahaded <abahaded@c3r9p6.1337.ma>
2025-05-15 19:12:13 +0100

    added getnextline

---
+++ helper_function/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_untilnewline(char *s);
+char	*ft_afternewline(char *s);
+size_t	ft_strlen(const char *s);
+char	*ft_strduup(const char *s);
+
+#endif
---
+++ helper_function/get_next_line/get_next_line_utils.c
@@ -0,0 +1,130 @@
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*p;
+	size_t	i;
+	size_t	j;
+	size_t	len;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	len = ft_strlen(s1) + ft_strlen(s2);
+	p = malloc(len + 1);
+	if (!p)
+		return (NULL);
+	while (s1[i])
+	{
+		p[i] = ((char *)s1)[i];
+		i++;
+	}
+	while (s2[j])
+		p[i++] = ((char *)s2)[j++];
+	p[i] = '\0';
+	return (p);
+}
+
+char	*ft_untilnewline(char *s)
+{
+	size_t	i;
+	char	*new;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (s[i] == '\n')
+		i++;
+	new = malloc(i + 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		new[i] = s[i];
+		i++;
+	}
+	if (s[i] == '\n')
+		new[i++] = '\n';
+	new[i] = '\0';
+	return (new);
+}
+
+char	*ft_afternewline(char *s)
+{
+	char	*new;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (!s[i])
+		return (NULL);
+	i++;
+	new = malloc(ft_strlen(s) - i + 1);
+	if (!new)
+		return (NULL);
+	j = 0;
+	while (s[i])
+		new[j++] = s[i++];
+	new[j] = '\0';
+	return (new);
+}
+
+// char	*ft_strchr(const char *s, int c)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	if (!s)
+// 		return (NULL);
+// 	while (s[i])
+// 	{
+// 		if (s[i] == (char)c)
+// 			return ((char *)&s[i]);
+// 		i++;
+// 	}
+// 	if (s[i] == (char)c)
+// 		return ((char *)&s[i]);
+// 	return (NULL);
+// }
+
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	j;
+
+// 	if (!s)
+// 		return (0);
+// 	j = 0;
+// 	while (s[j])
+// 		j++;
+// 	return (j);
+// }
+
+char	*ft_strduup(const char *s)
+{
+	size_t	len;
+	char	*p;
+	int		i;
+
+	len = ft_strlen(s);
+	i = 0;
+	p = malloc(len + 1);
+	if (!p)
+		return (NULL);
+	while (s[i])
+	{
+		p[i] = s[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
